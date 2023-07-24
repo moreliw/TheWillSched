794516c8fad99a6b72c713519fe6de680b454859
moreliw <williammorelli07@gmail.com>
2023-07-24 18:32:14 -0300

    costumers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CustomersComponent } from './customers/customers.component';
+import { SchedulingComponent } from './scheduling/scheduling.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'scheduling', component: SchedulingComponent },
+  { path: 'customers', component: CustomersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductStockListComponent } from './product-stock-list/product-stock-list.component';
 import { SharedComponentsModule } from './shared/shared-components/shared-components.module';
 import { CalendarComponent } from './calendar/calendar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UtilbarComponent } from './utilbar/utilbar.component';
+import { CustomersComponent } from './customers/customers.component';
+import { SchedulingComponent } from './scheduling/scheduling.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductStockListComponent,
     CalendarComponent,
     DashboardComponent,
     NavbarComponent,
     UtilbarComponent,
+    CustomersComponent,
+    SchedulingComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, SharedComponentsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedComponentsModule,
+    NgxPaginationModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
